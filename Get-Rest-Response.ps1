0e94929d361bac179dc7c7b6afa0d2aad57f8525
Ferdinando Esposito <ferdie@ferdie.co.uk>
2017-09-14 11:10:19 +0100

    first commit

---
+++ Get-Rest-Response.ps1
@@ -0,0 +1,25 @@
+﻿$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition;
+function Get-Rest-Response(){
+	. .\Get-Rest-URL.ps1;
+	$ocs = @{};
+	$response = "";
+	$uri = "";
+	$server = "";
+	$server = Get-Rest-URL;
+	$results = @{};
+	$ocs = Import-Csv -Path ("{0}{1}" -f $scriptPath,"\data\restCheckerData.CSV");
+	foreach($oc in $ocs.GetEnumerator()){
+		$uri = ("{1}/{0}/GBR" -f $oc.Value,$server); 	
+		try
+		{
+			$response = (Invoke-WebRequest -Uri $uri -DisableKeepAlive).statusCode
+		}
+		Catch
+		{	
+			$response = $_.Exception.Response.StatusCode.Value__;
+		}		
+		$results.Add($oc.Name, $response); 		
+	}
+	#Write-Output ($results);
+	return $results;
+}
---
+++ main.ps1
@@ -0,0 +1,14 @@
+﻿. .\Get-Rest-URL.ps1;
+. .\Get-Rest-Response.ps1;
+#Write-Output ("{0}: {1}" -f $oc.Name, $response);
+$results = Get-Rest-Response;
+foreach($result in $results.GetEnumerator()){
+	Start-Sleep -s 2;
+	if($result.Value -ne "200"){
+		Write-Host  ("{0}" -f $result.Name) -NoNewline; 
+		Write-Host  (": {0}" -f $result.Value) -ForegroundColor Red;
+	}else{
+		Write-Host  ("{0}" -f $result.Name) -NoNewline; 
+		Write-Host  (": {0}" -f $result.Value) -ForegroundColor Green;
+	}
+}
